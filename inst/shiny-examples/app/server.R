fa68dae8df843630b3cd0a859760da154c9e2391
neyhartj <neyhartje@gmail.com>
2018-06-13 10:49:26 -0500

    Some updates to dependencies

--- inst/shiny-examples/app/server.R
+++ inst/shiny-examples/app/server.R
@@ -7,16 +7,16 @@ source("source.R")
 
 
 shinyServer(function(input, output) {
-  
+
   # Run the simulation using inputs
   hwe_datasetInput <- eventReactive(eventExpr = input$hwe_action, valueExpr = {
-  
+
     # Run a simulation given the inputs
     p <- input$hwe_p
     t <- input$hwe_gen
     N <- input$hwe_pop_size
     r <- input$hwe_reps
-    
+
     # Return a list a results
     list(
       p = p,
@@ -25,50 +25,51 @@ shinyServer(function(input, output) {
       r = r,
       results = simpop(p = p, N = N, t = t, reps = r)
     )
-    
+
   })
-    
+
   # Plot the simulation
   output$hwe_plot <- renderPlot({
-    
+
     ## Color scheme from which to draw
-    col.scheme <- rainbow(n = 5)
-    
+    ## This is taken from the RcolorBrewer Set1
+    col.scheme <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00")
+
     data_set <- hwe_datasetInput()
-    
+
     # Plot
     par(xpd = TRUE, mar=c(9,4,4,2) + 0.1)
-    
+
     plot(NA, xlim = c(0,data_set$t), ylim = c(0,1), main = "Allele Frequencies Under Random Mating",
          xlab = "Generation", ylab = expression("Allele Frequency "~(italic(p))))
-    
+
     for (i in seq(data_set$r)) {
-      lines(x = data_set$results$gen, y = data_set$results[,i+1], col = col.scheme[i])
+      lines(x = data_set$results$gen, y = data_set$results[,i+1], col = col.scheme[i], lwd = 2)
     }
-    
+
     # Add legend
     legend(x = 0, y = -0.3, legend = paste("Rep", seq(data_set$r)), col = col.scheme, lwd = 2)
 
   })
-  
+
   # Add table
   output$hwe_table <- renderTable({
-    
+
     # Read-in the dataset input
     data_set <- hwe_datasetInput()
-    
+
     data.frame(
       Replication = paste("Rep", seq(data_set$r)),
       `Final Allele Frequency` = as.numeric(data_set$results[data_set$t,-1]),
-      `Fixation Generation` = apply(X = data_set$results[,-1], MARGIN = 2, FUN = function(gen) 
-        ifelse(any(gen == 0, gen == 1), which(gen == 0 | gen == 1)[1], NA)), 
-      
+      `Fixation Generation` = apply(X = data_set$results[,-1], MARGIN = 2, FUN = function(gen)
+        ifelse(any(gen == 0, gen == 1), which(gen == 0 | gen == 1)[1], NA)),
+
       check.names = FALSE, row.names = NULL
     )
-    
+
   }, align = "lcc", rownames = FALSE)
-  
-  
+
+
   ## Genetic variance tab
   # # Show theory if requested
   # observeEvent(input$var_theory, {
@@ -80,35 +81,35 @@ shinyServer(function(input, output) {
   #     )
   #   )
   # })
-  
+
   var_datasetInput <- reactive({
-    
+
     # Extract the variables
     ## Generate a sequence of allele frequencies
     ## Round in order to avoid weird bug in 'seq'
     p <- round(x = seq(0, 1, by = 0.01), digits = 2)
     q <- 1 - p
-    
+
     p_i <- input$var_p
     a <- input$var_a
     d <- input$var_d
     q_i <- 1 - p_i
-    
+
     # Calculate additive variance
     V_A <- 2 * p * q * ( ( a + d * (q - p) )^2 )
     V_D <- 4 * p^2 * q^2 * d^2
-    
+
     ## Calculate genetic variance at the specific gene frequency
     V_A_i <- V_A[p == p_i]
     V_D_i <- V_D[p == p_i]
-    
+
     # Total V_G is V_A + V_D
     V_G_tot <- V_A_i + V_D_i
     V_G_per <- 1
-    
+
     # Calculate proportions of V_A and V_D
     V_G_prop <- matrix(data = c(V_A_i, V_D_i), ncol = 1) / V_G_tot
-    
+
     list(
       p = p,
       p_i = p_i,
@@ -119,62 +120,62 @@ shinyServer(function(input, output) {
       V_G_per = V_G_per,
       V_G_prop = V_G_prop
     )
-    
+
   })
-  
+
   # Plot
   output$var_prop <- renderPlot({
-    
+
     # Get the data
     var_out <- var_datasetInput()
-    
+
     # Plot
-    bp <- barplot(height = var_out$V_G_prop, horiz = TRUE, beside = FALSE, 
+    bp <- barplot(height = var_out$V_G_prop, horiz = TRUE, beside = FALSE,
                   main = "Partition of Genetic Variance",
-                  xlab = expression(Proportion~of~V[G]), col = rainbow(n = 2),
+                  xlab = expression(Proportion~of~V[G]), col = c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00")[1:2],
                   legend.text = T)
-    
+
     # Position of text
     text_y <- apply(X = var_out$V_G_prop, MARGIN = 2, FUN = cumsum)
     text_y <- text_y - (var_out$V_G_prop/2)
-    
+
     # Add labels
     text(x = t(text_y), y = bp, labels = c(expression(bold(V[A])), expression(bold(V[D]))) )
-    
+
   })
-  
+
   ## Add another plot of V_A and V_D over allele frequencies
-  
+
   output$var_plot <- renderPlot({
-    
+
     # Get the data
     var_out <- var_datasetInput()
-    
+
     ## Colors of the genetic variance
-    colors <- rainbow(2)
-    
+    colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00")[1:2]
+
     # Plot
     ## Plot the total variance first
-    plot(x = var_out$p, y = var_out$V_A + var_out$V_D, type = "l", lwd = 2, 
+    plot(x = var_out$p, y = var_out$V_A + var_out$V_D, type = "l", lwd = 2,
          ylab = "Variance", xlab = expression("Allele Frequency"~(italic(p))))
     # Add lines for the dominance variance and additive variance
     lines(x = var_out$p, y = var_out$V_D, type = "l", col = colors[2], lwd = 2)
     lines(x = var_out$p, y = var_out$V_A, type = "l", col = colors[1], lwd = 2)
     # Add a vertical line at the allele frequency
     abline(v = var_out$p_i, lty = 2)
-    
+
     ## Add a legend
     legend(x = "topright", legend = c(expression(bold(V[A])), expression(bold(V[D])), expression(bold(V[G]))),
            col = c(rev(colors), "black"), lwd = 2)
-    
+
   })
-  
+
   # Table
   output$var_table <- renderTable({
-    
+
     # Get the data
     var_out <- var_datasetInput()
-    
+
     # Output data.frame
     data.frame(
       `Source of Variance` = c("Additive", "Dominance"),
@@ -183,16 +184,16 @@ shinyServer(function(input, output) {
       check.names = FALSE,
       row.names = NULL
     )
-    
+
   }, align = c("lcc"), rownames = FALSE)
-  
-  
+
+
   # Response to selection
   # Wait for input
   # observeEvent(eventExpr = input$resp_action, handlerExpr = {
-  
+
   resp_datasetInput <- eventReactive(eventExpr = input$resp_action, ignoreInit = TRUE, valueExpr = {
-    
+
     # Extract variables
     p <- input$resp_p
     # L <- input$resp_L
@@ -202,13 +203,13 @@ shinyServer(function(input, output) {
     N <- input$resp_n
     # t <- input$resp_t
     t <- 25
-    
+
     # Determine the additive effects of QTL
     a <- ((L - 1) / (L + 1)) ^ seq(L)
-    
+
     # If a is 0 (L = 1), set to 1
     a <- ifelse(a == 0, 1, a)
-    
+
     # Launch simulation
     # Return a list a results
     data_list <- list(
@@ -221,22 +222,22 @@ shinyServer(function(input, output) {
       a = a,
       results = simbreed(p = p, N = N, t = t, h = h, L = L, i = i, a = a)
     )
-    
+
     # Does 'resp_sim_results' exist? If so, append to it
     if (!exists("resp_sim_results")) {
       resp_sim_results <<- list(data_list)
-      
+
     } else {
       resp_sim_results <<- list(tail(resp_sim_results, 1)[[1]], data_list)
 
     }
-    
-    # Output the data    
+
+    # Output the data
     resp_sim_results
 
   })
-  
-  
+
+
   # Plot
   output$resp_plot <- renderPlot({
 
@@ -279,11 +280,11 @@ shinyServer(function(input, output) {
       lines(x = resp_out_old$results$gen, y = resp_out_old$results$G, lwd = 2.5,
             col = "grey75")
       legend(x = "bottomright", legend = c("Previous Run", "Current Run"), lwd = 2.5,
-             col = c("grey75", "blue"))
+             col = c("grey75", "#377EB8"))
     }
 
     ## Add lines
-    lines(x = resp_out$results$gen, y = resp_out$results$G, lwd = 2.5, col = "blue")
+    lines(x = resp_out$results$gen, y = resp_out$results$G, lwd = 2.5, col = "#377EB8")
 
 
 
@@ -297,11 +298,11 @@ shinyServer(function(input, output) {
       lines(x = resp_out_old$results$gen, y = resp_out_old$results$varG, lwd = 2.5,
             col = "grey75")
       legend(x = "topright", legend = c("Previous Run", "Current Run"), lwd = 2.5,
-             col = c("grey75", "red"))
+             col = c("grey75", "#E41A1C"))
     }
 
     ## Add lines
-    lines(x = resp_out$results$gen, y = resp_out$results$varG, lwd = 2.5, col = "red")
+    lines(x = resp_out$results$gen, y = resp_out$results$varG, lwd = 2.5, col = "#E41A1C")
 
 
 
@@ -315,55 +316,55 @@ shinyServer(function(input, output) {
       lines(x = gen, y = freq[,l], lwd = a_bin[l] / 2)
 
     }
-    
+
     # # Add a legend plot
     # plot(NA, ylim = c(-1, 1), xlim = c(-1, 1))
     # legend(x = 0, y = 0, legend = c("Current Simulation", "Previous Simulation"),
     #        col = "blue", "grey75", lwd = c(1,1))
 
   })
-  
-  
+
+
   # # Plot other
   # output$resp_supplot <- renderPlot({
-  #   
+  #
   #   # Get the data
   #   resp_out <- resp_datasetInput()
-  #   
+  #
   #   # Generations
   #   gen <- resp_out$results$gen
-  #   
+  #
   #   # Genetic variance over generations
   #   varG <- resp_out$results$varG
   #   # matrix of allele frequencies
   #   freq <- resp_out$results[,startsWith(x = colnames(resp_out$results), prefix = "p"), drop = FALSE]
   #   # Vector of allele effects
   #   a <- resp_out$a
-  #   
+  #
   #   # bins of allele effects
   #   a_bin <- .bincode(x = a, breaks = seq(0, 1, by = 0.1))
-  #   
-  #   
+  #
+  #
   #   # 2 plots
   #   par(mfrow = c(1, 2))
-  #   
-  #   plot(x = gen, y = varG, type = "l", main = "Genetic Variance Over Generations", 
+  #
+  #   plot(x = gen, y = varG, type = "l", main = "Genetic Variance Over Generations",
   #        ylab = "Standardized Genetic Variance", xlab = "Generation", lwd = 2.5,
   #        col = "red", ylim = c(0, 0.05), xlim = c(1, length(gen)))
-  #   
+  #
   #   # Plot for allele frequencies
   #   plot(NA, type = "l", main = "Allele Frequencies Over Generations",
   #        ylab = "Frequency", xlab = "Generation", ylim = c(0, 1), xlim = c(1, length(gen)))
-  #   
+  #
   #   # Iterate over allele frequencies
   #   for (l in seq(resp_out$L)) {
-  #     
+  #
   #     lines(x = gen, y = freq[,l], lwd = a_bin[l] / 2)
-  #     
+  #
   #   }
-  #   
+  #
   # })
-  
-  
-  
+
+
+
 }) # Close the server
--- inst/shiny-examples/app/ui.R
+++ inst/shiny-examples/app/ui.R
@@ -7,313 +7,310 @@ library(shiny)
 
 # Initialize the layout with a navbar
 fluidPage(
-  
-  ## Test the theme selector
-  theme = shinythemes::shinytheme("united"),
-  
+
   navbarPage(title = "Quantitative Genetics Simulations",
-             
+
              #### First tab ####
              tabPanel(title = "Introduction",
-                      
+
                       titlePanel("Introduction"),
-                            
+
                             mainPanel(
-                              
+
                               p("The complexity of quantitative genetics often requires intensive computation tools for analysis. Researchers can also use computers to run simulations of breeding programs. These can be especially informative for hypothesis testing or for decision-making in breeding. We will use simulations to better-understand the concepts of population genetics, genetic variance, and response to selection."),
-                              
+
                               br(),
-                              
+
                               h3("Learning Objectives"),
-                              
+
                               p("After completing this demo, you should be able to:"),
-                              
+
                               tags$li("  Understand how allele frequencies change over time in randomly-mating populations"),
                               tags$li("  Define genetic drift and understand the impact of genetic drift on breeding populations."),
                               tags$li("  Define the relationship between population size and the likelihood that alleles reach fixation"),
                               tags$li("  Understand the relationship between allele frequency and coded genotypic values on the levels of additive variance and dominance variance."),
                               tags$li("  Understand the relationship between response to selection, genetic variance, and allele frequencies."),
                               tags$li("  Define the impact of heritability and selection intensity on response to selection."),
-                              
+
                               br(),
-                              
+
                               h3("Navigation"),
-                              
+
                               p("This demo is very much 'point and click.' Click on the different tabs above to change the topic. Use the sliders to adjust the values of different parameters. Click the 'Simulate' button to run the simulations for topics 1 and 3."),
-                              
+
                               br(),
-                              
+
                               h4("Click on the first tab to begin!")
 
                             )
                    ),
-             
-             
-             
-             
+
+
+
+
              #### Second tab ####
              tabPanel("Randomly Mating Populations",
-                      
+
                       titlePanel("Randomly Mating Populations"),
-                            
+
                       # Create a sidebar panel
                       sidebarLayout(
-                        
+
                         sidebarPanel(
-                          
+
                           # Help text
                           helpText("This simulation will look at the changes in allele frequency for a single gene in a randomly-mating population."),
-                          
+
                           # Slider for p allele frequency
                           sliderInput(inputId = "hwe_p",
                                       "Starting Allele Frequency (p):",
                                       value = 0.5,
                                       min = 0.01,
                                       max = 0.99 ),
-                          
+
                           # Slider for number of individuals
                           sliderInput(inputId = "hwe_pop_size",
                                       "Breeding Population Size:",
                                       value = 100,
                                       min = 2,
                                       max = 2000 ),
-                          
-                          
+
+
                           # Slider for number of generations
                           sliderInput(inputId = "hwe_gen",
                                       "Number of Generations:",
                                       value = 200,
                                       min = 10,
                                       max = 1000 ),
-                          
+
                           # Input for the number of population replications
                           numericInput(inputId = "hwe_reps",
                                        "Number of Populations",
                                        value = 3,
                                        min = 1,
                                        max = 5 ),
-                          
+
                           # Break
                           br(),
-                          
+
                           # Helptext
                           helpText("Click to run the simulation."),
-                          
+
                           # Action button
                           actionButton(inputId = "hwe_action", label = "Simulate!")
-                          
+
                         ),
-                        
+
                         # main panel
                         mainPanel(
-                          
+
                           # Use a fluid row to center everything
                           fluidRow(
-                            column(width = 12, align = "center", 
+                            column(width = 12, align = "center",
                                    # Add graph
                                    plotOutput(outputId = "hwe_plot"),
-                                   
+
                                    # Table
                                    tableOutput(outputId = "hwe_table")
-                                   
+
                             )
-                            
+
                           )
-                          
+
                         )
                       ) # End the sidebar layout
-                      
-              # End the tab        
+
+              # End the tab
              ),
-             
-             
-             
+
+
+
              #### Third tab ####
              tabPanel("Genetic Variance",
-                      
+
                       titlePanel("Genetic Variance"),
-                      
+
                       # Create a sidebar panel
                       sidebarLayout(
-                        
+
                         sidebarPanel(
-                          
+
                           # Help text
-                          helpText("This simulation will assess the impact of allele 
-                                   frequency and coded genotypic values on the level of 
+                          helpText("This simulation will assess the impact of allele
+                                   frequency and coded genotypic values on the level of
                                    genetic variance at a single gene."),
-                          
+
                           # Slider for p allele frequency
                           sliderInput(inputId = "var_p",
                                       "Starting Allele Frequency (p):",
                                       value = 0.5,
                                       min = 0,
-                                      max = 1, 
+                                      max = 1,
                                       step = 0.01),
-                          
+
                           # Slider for additive effect
                           sliderInput(inputId = "var_a",
                                       "Additive Effect (a):",
-                                      value = 1, 
-                                      min = 0, 
+                                      value = 1,
+                                      min = 0,
                                       max = 10 ),
-                          
+
                           # Slider for dominance effect
                           sliderInput(inputId = "var_d",
                                       "Dominance Effect (d):",
-                                      value = 1, 
-                                      min = 0, 
+                                      value = 1,
+                                      min = 0,
                                       max = 10 )
-                          
+
                         ),
-                        
+
                         mainPanel(
-                          
+
                           # Use a fluid row to center everything
                           fluidRow(
-                            column(width = 6, align = "center", 
-                                   
+                            column(width = 6, align = "center",
+
                                    # Add graph
                                    plotOutput(outputId = "var_prop"),
-                            
+
                                    # Table
                                    tableOutput(outputId = "var_table")),
-                            
+
                             # Second column
                             column(width = 6, align = "center",
-                                   
+
                                    # Add a graph
                                    plotOutput(outputId = "var_plot"))
-                            
+
                           )
-                          
+
                         )
-                        
+
                       )
-              
-              # Close the tab        
+
+              # Close the tab
              ),
-             
-             
-             
+
+
+
              #### Fourth tab ####
              tabPanel("Response to Selection",
-                      
+
                       titlePanel("Response to Selection"),
-                      
+
                       # Create a sidebar panel
                       sidebarLayout(
-                        
+
                         sidebarPanel(
-                          
+
                           # Help text
                           helpText("In this simulation, we will explore how different parameters impact the response to selection and other measurements for a trait influenced by many genes."),
-                          
+
                           # Slider for p allele frequency
                           sliderInput(inputId = "resp_p",
                                       "Starting Allele Frequency (p):",
                                       value = 0.5,
                                       min = 0,
                                       max = 1 ),
-                          
+
                           # # Slider for number of genes
                           # sliderInput(inputId = "resp_L",
                           #             "Number of genes:",
-                          #             value = 20, 
-                          #             min = 1, 
+                          #             value = 20,
+                          #             min = 1,
                           #             max = 100 ),
-                          
+
                           # Slider for heritability
                           sliderInput(inputId = "resp_h",
                                       "Heritability:",
-                                      value = 0.5, 
-                                      min = 0, 
+                                      value = 0.5,
+                                      min = 0,
                                       max = 1 ),
-                          
+
                           helpText("The selection intensity is defined as the proportion of individuals selected from one generation to create the next generation. A low proportion of selected individuals is a high selection intensity, and vice versa."),
-                          
+
                           # Slider for selection intensity
                           sliderInput(inputId = "resp_i",
                                       "Selection Intensity:",
-                                      value = 0.1, 
-                                      min = 0, 
+                                      value = 0.1,
+                                      min = 0,
                                       max = 1 ),
-                          
+
                           # Slider for population size
                           sliderInput(inputId = "resp_n",
                                       "Breeding Population Size:",
                                       value = 100,
                                       min = 2,
                                       max = 2000 ),
-                          
+
                           # # Slider for number of generations
                           # sliderInput(inputId = "resp_t",
                           #             "Number of Breeding Generations:",
-                          #             value = 10, 
-                          #             min = 1, 
+                          #             value = 10,
+                          #             min = 1,
                           #             max = 50 ),
-                          
+
                           # Break
                           br(),
-                          
+
                           # Helptext
                           helpText("Click to run the simulation."),
-                          
+
                           # Action button
                           actionButton(inputId = "resp_action", label = "Simulate!")
-                          
-                          
-                          
+
+
+
                         ),
-                        
+
                         mainPanel(
-                          
+
                           # Add graph
                           plotOutput(outputId = "resp_plot", height = 700)
-                          
+
                           # Depricated
                           # # Add another graph
                           # plotOutput(outputId = "resp_supplot"),
-                          # 
+                          #
                           # # Table
                           # tableOutput(outputId = "resp_table")
-                          
+
                         )
-                        
+
                       )
              ),
-             
-             
-             
-             
+
+
+
+
              #### Fifth tab ####
              tabPanel(title = "Theory",
-                        
+
                         withMathJax(
                           includeHTML("theory.html")
                         )
              )
-             
+
    # End the navigation bar
   )
-  
+
 ) # End the UI
-                                      
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-                          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
